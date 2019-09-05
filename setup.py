1a4e9e7c8cfe27236478525f5675d8fed41c9b89
Ralph Samer <rsamer@users.noreply.github.com>
2019-09-05 12:49:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ REQUIRES = ["connexion"]
 setup(
     name=NAME,
     version=VERSION,
-    description="OpenReq Similar/Related Requirements Recommendation Service",
+    description="Issue Recommendation Service for Eclipse Plugin",
     author_email="rsamer@ist.tugraz.at",
     url="",
-    keywords=["Swagger", "OpenReq Similar/Related Requirements Recommendation Service"],
+    keywords=["Swagger", "Issue Recommendation Service for Eclipse Plugin"],
     install_requires=REQUIRES,
     packages=find_packages(),
     package_data={'': ['swagger/swagger.yaml']},
@@ -29,7 +29,7 @@ setup(
     entry_points={
         'console_scripts': ['application=application.__main__:main']},
     long_description="""\
-    OpenReq Similar/Related Requirements Recommendation Service
+    Issue Recommendation Service for Eclipse Plugin
     """
 )
 
